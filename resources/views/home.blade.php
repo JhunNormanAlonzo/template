e713031054faff8caa5162c1e8f444bdfe7fbbec
dvxgit-jalonzo <jalonzo@diavox.net>
2024-10-28 07:20:42 +0800

    gna template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<x-navbar></x-navbar>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('page_title')
+   Students
+@endsection
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<x-sidebar></x-sidebar>
+
+@section('main')
+
+    <div class="row">
+        <div class="col-12">
+            <x-btn class="float-end">Add Students</x-btn>
+        </div>
+        <div class="col-12">
+            <x-table>
+                <x-thead>
+                    <x-th>First Name</x-th>
+                    <x-th>Middle Name</x-th>
+                    <x-th>Last Name</x-th>
+                    <x-th>Course</x-th>
+                    <x-th>Action</x-th>
+                    <x-th></x-th>
+                </x-thead>
+                <x-tbody>
+                    <x-tr>
+                        <x-td>Jhun</x-td>
+                        <x-td>Norman</x-td>
+                        <x-td>Alonzo</x-td>
+                        <x-td>BSIT</x-td>
+
+                         <x-td>
+                            <i class="btn btn-warning bi bi-pencil"></i>
+                            <i class="btn btn-danger bi bi-trash"></i>
+                        </x-td>
+                    </x-tr>   <x-tr>
+                        <x-td>Jhun</x-td>
+                        <x-td>Norman</x-td>
+                        <x-td>Alonzo</x-td>
+                        <x-td>BSIT</x-td>
+                         <x-td>
+                            <i class="btn btn-warning bi bi-pencil"></i>
+                            <i class="btn btn-danger bi bi-trash"></i>
+                        </x-td>
+                    </x-tr>   <x-tr>
+                        <x-td>Jhun</x-td>
+                        <x-td>Norman</x-td>
+                        <x-td>Alonzo</x-td>
+                        <x-td>BSIT</x-td>
+                         <x-td>
+                            <i class="btn btn-warning bi bi-pencil"></i>
+                            <i class="btn btn-danger bi bi-trash"></i>
+                        </x-td>
+                    </x-tr>   <x-tr>
+                        <x-td>Jhun</x-td>
+                        <x-td>Norman</x-td>
+                        <x-td>Alonzo</x-td>
+                        <x-td>BSIT</x-td>
+                         <x-td>
+                            <i class="btn btn-warning bi bi-pencil"></i>
+                            <i class="btn btn-danger bi bi-trash"></i>
+                        </x-td>
+                    </x-tr>
+                    <x-tr>
+                        <x-td>Jhun</x-td>
+                        <x-td>Norman</x-td>
+                        <x-td>Alonzo</x-td>
+                        <x-td>BSIT</x-td>
+                         <x-td>
+                            <i class="btn btn-warning bi bi-pencil"></i>
+                            <i class="btn btn-danger bi bi-trash"></i>
+                        </x-td>
+                    </x-tr>
+
+                </x-tbody>
+            </x-table>
         </div>
     </div>
-</div>
+
 @endsection
